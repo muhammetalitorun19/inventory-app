0db6d1da16dfada44da7d5253eda9e62b1af06da
Muhammet Ali Torun <muhammetalitorun19@gmail.com>
2025-04-14 15:30:54 +0300

    mat30

--- src/main/java/com/example/inventory/controller/HealthCheckController.java
+++ src/main/java/com/example/inventory/controller/HealthCheckController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String healthCheck() {
         return "OK ✅";
     }
 }
-
