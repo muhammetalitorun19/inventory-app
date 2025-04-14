73b8c46e593a887cd48b5425f2e8eeb816425158
Muhammet Ali Torun <muhammetalitorun19@gmail.com>
2025-04-14 10:52:22 +0300

    mat4

--- src/main/java/com/example/inventory/config/WebConfig.java
+++ src/main/java/com/example/inventory/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:8080",
+                                "http://localhost:4200",
                                 "https://inventory-app-production-76ef.up.railway.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
