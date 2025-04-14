ff41bf49b94dc0d8e0fede444712d7ff9ad303fc
Muhammet Ali Torun <muhammetalitorun19@gmail.com>
2025-04-14 14:40:14 +0300

    mat24

--- src/main/java/com/example/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/InventoryApplication.java
@@ -2,14 +2,11 @@ package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.inventory.model")
 public class InventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
     }
 }
+
